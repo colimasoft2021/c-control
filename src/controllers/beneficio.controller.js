6ee409e2899c3b26b1fa520d068ac6208f982eef
colimasoft <colimasoft_dev@colimasoft.com>
2022-02-15 13:37:18 -0600

    update controller

--- src/controllers/beneficio.controller.js
+++ src/controllers/beneficio.controller.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getConnection, querys, sql } from "../database/connection";
+import { getConnection, querys, sql } from "../database/index";
 
 const obtenerConfiguracion = (req, res) => {
     res.json('test');
@@ -13,7 +13,7 @@ const bitacoraBeneficio = (req, res) => {
 
 }
 
-const acumulacionComponenteCentral = (req, res) => {
+const acumulacionComponenteCentral = async(req, res) => {
     console.log(req.body);
     const {
         id_cliente,
@@ -25,16 +25,35 @@ const acumulacionComponenteCentral = (req, res) => {
         TipoMovimiento
     } = req.body;
     let msgProcesoNoTerminado = {CodigoEstatus: "", Mensaje: ""};
-    let MonId = desencriptarBase64(Monedero);
-    let infoMonedero = obtenerStatusMonedero(MonId);
-    let statusMonedero = infoMonedero.CodigoEstatus;
-    let msgMonedero = infoMonedero.Mensaje;
+    let idMonedero = desencriptarBase64(Monedero);
+    let infoMonedero = await obtenerStatusMonedero(idMonedero);
+    let statusMonedero = infoMonedero.data.CodigoEstatus;
+    let msgMonedero = infoMonedero.data.Mensaje;
     if(statusMonedero !== "00") {
         msgProcesoNoTerminado.CodigoEstatus = CodigoEstatus;
         msgProcesoNoTerminado.Mensaje = msgMonedero;
         res.send(msgProcesoNoTerminado);
     }
+    let resMonederoTransDb = await insertarMonederoTransDb(id_cliente, TipoMovimiento, Monto, Monedero);
+    console.log(resMonederoTransDb);
+    let resMonederoOmniTrans = {};
+    let StatusDb = "";
+    if(TipoMovimiento === 'I') {
+        StatusDb = "ACUMULADO";
+        resMonederoOmniTrans = await acumularMonederoOmniTrans();
+    }else if(TipoMovimiento === 'D') {
+        StatusDb = "REDIMIDO";
+        resMonederoOmniTrans = await devolverMonederoOmniTrans();
+    }else if(TipoMovimiento === 'R') {
+        StatusDb = "REDIMIDO";
+        resMonederoOmniTrans = await redimirMonederoOmniTrans();
+    }
     
+    
+    //if(resMonederoOmniTrans.Codigoestatus === '00'){
+        StatusDb = "ACUMULADO";
+    //}
+    //    actualizarEstatusMonederoTransDb('ACUMULADO');
 }
 
 const desencriptarBase64 = (encrypted) => {
@@ -43,41 +62,101 @@ const desencriptarBase64 = (encrypted) => {
     return decoded;
 }
 
-const obtenerStatusMonedero = async(monId) => {
-    const res = await axios.get(`http://10.0.15.80/apiOmnitransGlobal/api/Omnitrans?MonId=${monId}`);
-    console.log(res);
+const obtenerStatusMonedero = async(idMonedero) => {
+    const res = await axios.get(`http://10.0.15.80/apiOmnitransGlobal/api/Omnitrans?MonId=${idMonedero}`);
     return res;
 }
 
-const insertarMonederoTransDb = async(datos) => {
+const insertarMonederoTransDb = async(id_cliente, TipoMovimiento, Monto, Monedero) => {
     let tipoMov = "";
-    if(datos.TipoMovimiento === "D" || datos.TipoMovimiento === "I"){
+    if(TipoMovimiento === "D" || TipoMovimiento === "I"){
         tipoMov = "ACUMULADO";
     }else{
         tipoMov = "REDIMIDO";
     }
+    let fechaHora = new Date();
+    let ultimaTransaccion = await obtenerUltimaTransaccionMonedero(Monedero);
+    console.log(ultimaTransaccion);
+    let transaccion = null;
+    if(ultimaTransaccion.length > 0 || ultimaTransaccion !== undefined) {
+        transaccion = parseInt(ultimaTransaccion.Transaccion) + 1;
+        transaccion = ("000" + transaccion).slice(-4);
+        console.log(transaccion);
+    }
+    console.log('t',transaccion);
     try {
         const pool = await getConnection();
-    
         await pool
           .request()
-          .input("Id_cliente", sql.VarChar, idCliente)
+          .input("Id_Cliente", sql.VarChar, id_cliente)
           .input("FechaHora", sql.DateTime, fechaHora)
           .input("TipoMov", sql.VarChar, tipoMov)
-          .input("Id_Monedero", sql.VarChar, idMonedero)
-          .input("Monto", sql.Decimal(10, 2), monto)
+          .input("Id_Monedero", sql.VarChar, Monedero)
+          .input("Monto", sql.Decimal(10, 2), Monto)
+          .input("Transaccion", sql.VarChar, transaccion.toString())
           .query(querys.insertMonederoDb);
-    
-        res.json({ Id_Cliente, FechaHora, TipoMov, Id_monedero, Monto });
+
+        const insertado = await pool
+          .request()
+          .input("Id_Monedero", Monedero)
+          .query(querys.obtenerUltimaTransaccion);
+
+        return (insertado.recordset[0]);
     } catch (error) {
-        res.status(500);
-        res.send(error.message);
+        return error.message;
     }
 }
 
-const insertarMonederoOmniTrans = () => {
-
+const acumularMonederoOmniTrans = async() => {
+    let result = await axios.put('http://10.0.15.80/apiOmnitransGlobal/api/Omnitrans',{
+        Monedero: idMonedero,
+        Caja: caja,
+        sucursal: sucursal,
+        Cajero: cajero,
+        Transaccion: transaccion,
+        Monto: Monto,
+    });
+    return result;
 }
+
+const devolverMonederoOmniTrans =  async() => {
+    let result = await axios.put('http://10.0.15.80/apiOmnitransGlobal/api/Devolucion',{
+        Monedero: idMonedero,
+        Caja: caja,
+        sucursal: sucursal,
+        Cajero: cajero,
+        Transaccion: transaccion,
+        Monto: Monto,
+    });
+    return result;
+}
+
+const redimirMonederoOmniTrans =  async() => {
+    let result = await axios.put('http://10.0.15.80/apiOmnitransGlobal/api/Omnitrans',{
+        Monedero: idMonedero,
+        Caja: caja,
+        sucursal: sucursal,
+        Cajero: cajero,
+        Transaccion: transaccion,
+        Monto: Monto,
+    });
+    return result;
+}
+
+const obtenerUltimaTransaccionMonedero = async(Monedero) => {
+    try {
+        const pool = await getConnection();
+    
+        const result = await pool
+          .request()
+          .input("Id_Monedero", Monedero)
+          .query(querys.obtenerUltimaTransaccion);
+        return result.recordset[0];
+    } catch (error) {
+        return error.message;
+    }
+}
+
 module.exports = {
     obtenerConfiguracion,
     calcularBeneficio,
--- src/database/querys.js
+++ src/database/querys.js
@@ -1,5 +1,4 @@
 export const querys = {
-    getProducById: "SELECT * FROM Products Where Id = @Id",
-    insertMonederoDb:
-      "INSERT INTO Monedero_Trans (Id_Cliente, FechaHora, TipoMov, Id_Monedero, Monto) VALUES (@idCliente,@fechaHora,@tipoMov,@idMonedero,@Monto);",
-  };
+  obtenerUltimaTransaccion: "SELECT TOP (1) * FROM Monedero_Trans WHERE Id_Monedero = @Id_Monedero ORDER BY Id_Trans DESC",
+  insertMonederoDb: "INSERT INTO Monedero_Trans (Id_Cliente, FechaHora, TipoMov, Id_Monedero, Monto, Transaccion) VALUES (@Id_Cliente,@FechaHora,@TipoMov,@Id_Monedero,@Monto,@Transaccion);",
+};
