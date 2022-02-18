bdd601ea15a90ee6c65446e14b4f375586d728ed
colimasoft <colimasoft_dev@colimasoft.com>
2022-02-17 23:04:30 -0600

    agregar query para obtener la configuracion

--- src/database/querys.js
+++ src/database/querys.js
@@ -5,4 +5,5 @@ export const querys = {
   insertClienteBitacoraBeneficio: "INSERT INTO Bitacora_BeneficioCF (tipo_cliente, id_clienteapp, correo, tipo_proceso, id_pedido, monto_total, fecha_pago, forma_pago, monto_acum, estatus_movimiento, id_monedero, caja) VALUES (@tipo_cliente, @id_clienteapp, @correo, @tipo_proceso, @id_pedido, @monto_total, @fecha_pago, @forma_pago, @monto_acum, @estatus_movimiento, @id_monedero, @caja);",
   obtenerUltimaTransaccionBitacora: "SELECT TOP (1) * FROM Bitacora_BeneficioCF WHERE Id_Monedero = @Id_Monedero ORDER BY id DESC",
   actualizarBitacoraBeneficio: "UPDATE Bitacora_BeneficioCF SET Estatus_Movimiento = @Estatus_Movimiento WHERE id = @id",
+  obtenerConfiguracion: "SELECT * FROM Configuraciones WHERE Clave = @Clave",
 };
